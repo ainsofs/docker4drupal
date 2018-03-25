32b063d6a3bd82b7b4a2977318acdbe482f1d32f
Ainsof So'o <ainsof.soo@gmail.com>
2018-03-24 14:18:19 -1100

    add memcache settings

--- settings.docker.php
+++ settings.docker.php
@@ -16,12 +16,14 @@ $databases = array(
   ),
 );
 
+# varnish settings
 #$conf['varnish_version'] = 4;
 #$conf['varnish_control_terminal'] = 'varnish:6082';
 #$conf['varnish_control_key'] = 'secret';
 
 #$contrib_path = is_dir('sites/all/modules/contrib') ? 'sites/all/modules/contrib' : 'sites/all/modules';
 
+# redis settings
 #$conf['redis_client_base'] = 0;
 #$conf['redis_client_interface'] = 'PhpRedis';
 #$conf['lock_inc'] = $contrib_path . '/redis/redis.lock.inc';
@@ -32,3 +34,14 @@ $databases = array(
 #$conf['redis_client_host'] = 'redis';
 #$conf['redis_client_port'] = '6379';
 
+# memcache settings
+#$conf['memcache_storage_key_prefix'] = 'pipap_docker';
+#$conf['cache_backends'][] = $contrib_path . '/memcache/memcache.inc';
+#$conf['lock_inc'] = $contrib_path . '/memcache/memcache-lock.inc';
+#$conf['cache_default_class'] = 'MemCacheDrupal';
+#$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+#$conf['page_cache_without_database'] = TRUE;
+#$conf['page_cache_invoke_hooks'] = FALSE;
+#$conf['memcache_extension'] = 'memcached';
+#$conf['memcache_stampede_protection'] = TRUE;
+#$conf['memcache_servers'] = array('memcached:11211' => 'default');
